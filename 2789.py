e068a6a6b4b30ff2b45fb0bdf06d15489910ce06
Hyojin <0910.shj@gmail.com>
2022-05-16 22:13:49 +0900

    Update 2789.py

--- 2789.py
+++ 2789.py
@@ -1,13 +1,27 @@
-from itertools import combinations
+# from itertools import combinations
+
+# n, m = map(int, input().split())
+# cards = list(map(int, input().split()))
+
+# combs = list(combinations(cards, 3))
+
+# max_sum = 0
+# for comb in combs:
+#     if sum(comb) > max_sum and sum(comb) <= m:
+#         max_sum = sum(comb)
+
+# print(max_sum)
 
 n, m = map(int, input().split())
 cards = list(map(int, input().split()))
 
-combs = list(combinations(cards, 3))
+result = 0
 
-max_sum = 0
-for comb in combs:
-    if sum(comb) > max_sum and sum(comb) <= m:
-        max_sum = sum(comb)
-
-print(max_sum)
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if cards[i]+cards[j]+cards[k] > m:
+                continue
+            else:
+                result = max(result, cards[i]+cards[j]+cards[k])
+print(result)
