73efc78171e83a8000a4babf22d6dac9e49e43af
Hyojin <0910.shj@gmail.com>
2022-05-21 23:37:08 +0900

    220521

--- 1316.py
+++ 1316.py
@@ -1,29 +1,46 @@
+# n = int(input())
+# group_word = n
+
+# for _ in range(n):
+#     word = input()
+
+#     if len(word) <= 2:
+#         continue
+
+#     count = list(0 for i in range(123))
+
+#     c = word[0]
+#     count[ord(c)] += 1
+#     i = 0
+#     while(True):
+#         if word[i+1] == c:
+#             i += 1
+#             continue
+#         else:
+#             c = word[i+1]
+#             if count[ord(c)] != 0:
+#                 group_word -= 1
+#                 break
+#             count[ord(c)] += 1
+#             i += 1
+#         if i == len(word)-1:
+#             break
+
+# print(group_word)
+
 n = int(input())
-group_word = n
+cnt = 0
 
-for _ in range(n):
+for i in range(n):
+    alphabet = [0 for _ in range(123)]
     word = input()
-
-    if len(word) <= 2:
-        continue
-
-    count = list(0 for i in range(123))
-
-    c = word[0]
-    count[ord(c)] += 1
-    i = 0
-    while(True):
-        if word[i+1] == c:
-            i += 1
-            continue
-        else:
-            c = word[i+1]
-            if count[ord(c)] != 0:
-                group_word -= 1
+    for a in range(len(word)):
+        if alphabet[ord(word[a])] != 0:
+            if word[a] != word[a-1]:
+                cnt += 1
                 break
-            count[ord(c)] += 1
-            i += 1
-        if i == len(word)-1:
-            break
+            else:
+                continue
+        alphabet[ord(word[a])] += 1
 
-print(group_word)
+print(n-cnt)
