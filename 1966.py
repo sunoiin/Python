eac0a633e489af3b73d1f9d9da7c68a9f783f9d7
Hyojin <0910.shj@gmail.com>
2022-05-25 16:31:35 +0900

    Update 1966.py

--- 1966.py
+++ 1966.py
@@ -5,11 +5,26 @@ test_case = int(stdin.readline())
 for _ in range(test_case):
     # n: 문서의 개수
     # m: 몇 번째로 인쇄되었는지 궁금한 문서
-    # importance: 각 문서의 중요도
+    # imp: 각 문서의 중요도
+    # idx: 인쇄 명령을 받은 순서
     n, m = map(int, stdin.readline().split())
-    importance = list(map(int, stdin.readline().split()))
-    order = 1
-    for i in importance:
-        if i > importance[m]:
+    imp = list(map(int, stdin.readline().split()))
+    idx = [i for i in range(n)]
+
+    order = 0
+
+    while(True):
+        if imp[0] == max(imp):
             order += 1
-    print(order)
+
+            if idx[0] != m:
+                del imp[0]
+                del idx[0]
+            else:
+                print(order)
+                break
+        else:
+            imp.append(imp[0])
+            idx.append(idx[0])
+            del imp[0]
+            del idx[0]
