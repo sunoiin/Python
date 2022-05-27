44d550e5e90170ad8209127905c1489e4c055ed7
Hyojin <0910.shj@gmail.com>
2022-05-27 18:42:47 +0900

    Update 11866.py

--- 11866.py
+++ 11866.py
@@ -1,14 +1,18 @@
-from collections import deque
-n, k = map(int, input().split())
-s = deque([])
-for i in range(1, n + 1):
-    s.append(i)
-print('<', end='')
-while s:
-    for i in range(k - 1):
-        s.append(s[0])
-        s.popleft()
-    print(s.popleft(), end='')
-    if s:
-        print(', ', end='')
-print('>')
+N, K = map(int, input().split())
+people = [i for i in range(1, N+1)]
+
+remove_people = K-1
+cnt = 0
+print("<", end="")
+while(cnt < N):
+    cnt += 1
+    K_cnt = 0
+    while(K_cnt < K-1):
+        people.append(people[0])
+        people.pop(0)
+        K_cnt += 1
+    if cnt >= N:
+        print(str(people.pop(0)), end="")
+    else:
+        print(str(people.pop(0))+", ", end="")
+print(">")
