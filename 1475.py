b6e143473cce2953bf41628ac14e32b32237cc14
Hyojin <0910.shj@gmail.com>
2022-05-30 00:05:37 +0900

    Update 1475.py

--- 1475.py
+++ 1475.py
@@ -7,9 +7,8 @@ count = [0 for _ in range(10)]
 for i in N:
     count[i] += 1
 
-six_nine = math.ceil((count[6]+count[9])/2)
 if max(count) == count[6] or max(count) == count[9]:
-    count[6] = six_nine
-    count[9] = six_nine
+    six_nine = math.ceil((count[6]+count[9])/2)
+    count[6], count[9] = six_nine, six_nine
 
 print(max(count))
