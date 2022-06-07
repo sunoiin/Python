f667b87d9caea13ae5eb77ec3469cda684478307
Hyojin <0910.shj@gmail.com>
2022-06-08 00:49:55 +0900

    Update 42862.py

--- Programmers/Lv1/42862.py
+++ Programmers/Lv1/42862.py
@@ -1,6 +1,14 @@
 def solution(n, lost, reserve):
-    net_lost = set(lost)-set(reserve)
-    net_reserve = set(reserve)-set(lost)
+    # set 자료형
+    # net_lost=set(lost)-set(reserve)
+    # net_reserve=set(reserve)-set(lost)
+
+    # list 자료형
+    lost = sorted(lost)
+    reserve = sorted(reserve)
+
+    net_lost = [i for i in lost if i not in reserve]
+    net_reserve = [i for i in reserve if i not in lost]
     answer = n-len(net_lost)
 
     for lost in net_lost:
@@ -13,6 +21,6 @@ def solution(n, lost, reserve):
     return answer
 
 
-print(solution(5, [2, 4], [1, 3, 5]))
-print(solution(5, [2, 4], [3]))
-print(solution(3, [3], [1]))
+print(solution(5, [2, 4], [1, 3, 5]))  # 5
+print(solution(5, [2, 4], [3]))  # 4
+print(solution(3, [3], [1]))  # 2
