d613737341fe4a241a4d77848fdafd109294d2ee
Hyojin <0910.shj@gmail.com>
2022-06-19 23:50:41 +0900

    .

---
+++ Programmers/12910.py
@@ -0,0 +1,8 @@
+def solution(arr, divisor):
+    answer = sorted([i for i in arr if i % divisor == 0])
+    return answer if answer else [-1]
+
+
+print(solution([5, 9, 7, 10], 5))  # [5, 10]
+print(solution([2, 36, 1, 3], 1))  # [1, 2, 3, 36]
+print(solution([3, 2, 6], 10))  # [-1]
--- Programmers/17682.py
+++ Programmers/17682.py
@@ -3,19 +3,35 @@ def solution(dartResult):
     num = ''
     area = {'S': 1, 'D': 2, 'T': 3}
     for i in range(len(dartResult)):
+        # 숫자가 나오면 숫자를 변수 num에 저장
         if dartResult[i].isdigit():
             num += dartResult[i]
+        # 숫자가 아닌 문자일 때
         else:
+            # 문자가 S/D/T라면 숫자를 거듭제곱하여 stack에 추가
             if dartResult[i] in area:
-                stack.append(int(dartResult[i]) ** area[dartResult[i]])
-                if dartResult[i] == '*':
-                    stack[-1] = 2 * stack[-1]
-                    if len(stack) > 1:
-                        stack[-2] = 2 * stack[-2]
-                elif dartResult[i] == '#':
-                    stack[-1] = -stack[-1]
-                print(dartResult[i], stack)
-            print(num, stack)
+                stack.append(int(num) ** area[dartResult[i]])
+
+            # 문자가 */#라면 그 전까지의 점수들을 상에 맞게 처리
+            if dartResult[i] == '*':
+                stack[-1] = 2 * stack[-1]
+                if len(stack) > 1:
+                    stack[-2] = 2 * stack[-2]
+            elif dartResult[i] == '#':
+                stack[-1] = -stack[-1]
+            print(dartResult[i], stack)
+
+            # 숫자를 초기화
             num = ''
+
     answer = sum(stack)
     return answer
+
+
+print(solution('1S2D*3T'))
+# print(solution('1D2S#10S'))
+# print(solution('1D2S0T'))
+# print(solution('1S*2T*3S'))
+# print(solution('1D#2S*3S'))
+# print(solution('1T2D3D#'))
+# print(solution('1D2S3T*'))
