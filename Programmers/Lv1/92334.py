2110ea547852a38cc8643642172c4800e76d6901
Hyojin <0910.shj@gmail.com>
2022-06-23 00:22:03 +0900

    Update 92334.py

--- Programmers/Lv1/92334.py
+++ Programmers/Lv1/92334.py
@@ -1,32 +1,44 @@
 def solution(id_list, report, k):
-    answer = []
-    blocked_id = []
-    set_report = set(report)
-    report_cnt = {id: 0 for id in id_list}
-    mail_cnts = {id: 0 for id in id_list}
+    # 동일한 유저에 대한 신고는 1회로 처리 -> report 중복 제거 필요
+    # k번 이상 신고된 유저는 정지, 신고한 유저에게 메일로 발송
 
-    for id in set_report:
+    # 1. 유저의 신고받은 횟수, 메일받을 횟수 딕셔너리 초기화
+    report_cnt = {id: 0 for id in id_list}
+    mail_cnt = {id: 0 for id in id_list}
+
+    # 2. 집합을 이용하여 중복 없는 report 생성
+    report = set(report)
+
+    # 3. 신고 받은 횟수를 딕셔너리에 저장
+    for id in report:
         report_cnt[id.split()[1]] += 1
 
-    for i in report_cnt:
-        if report_cnt.get(i) >= k:
-            blocked_id.append(i)
+    # 4. 신고 받은 횟수가 k 이상이라면, blocked_id에 추가
+    blocked_id = []
+    for id, cnt in report_cnt.items():
+        if cnt >= k:
+            blocked_id.append(id)
 
-    for i in set_report:
+    # 5. 신고 리스트에서, blocked_id를 신고한 사람이 메일 받는 횟수 추가
+    for i in report:
         if i.split()[1] in blocked_id:
-            mail_cnts[i.split()[0]] += 1
+            mail_cnt[i.split()[0]] += 1
 
-    answer = list(mail_cnts.values())
-    print(answer)
+    answer = list(mail_cnt.values())
+
     return answer
 
 
-id_list = ["muzi", "frodo", "apeach", "neo"]
-report = ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"]
-k = 2
+print(solution(
+    ["muzi", "frodo", "apeach", "neo"],
+    ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"],
+    2
+))
+# [2, 1, 1, 0]
 
-# id_list = ["con", "ryan"]
-# report = ["ryan con", "ryan con", "ryan con", "ryan con"]
-# k = 3
-
-solution(id_list, report, k)
+print(solution(
+    ["con", "ryan"],
+    ["ryan con", "ryan con", "ryan con", "ryan con"],
+    3
+))
+# [0, 0]
